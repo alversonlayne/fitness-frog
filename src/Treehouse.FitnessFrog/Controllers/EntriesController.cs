49aefc001fcd3bc8f311ff0e509125f44fb43454
Alverson Layne <alverson.layne2@gmail.com>
2020-01-25 21:13:56 -0500

    Updated the "Add Entry" controller and view to use the Entry data model

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -41,25 +41,24 @@ namespace Treehouse.FitnessFrog.Controllers
 
         public ActionResult Add()
         {
-            return View();
+            var entry = new Entry()
+            {
+                Date = DateTime.Today
+            };
+            
+            return View(entry);
         }
 
         [HttpPost]
-        public ActionResult Add(DateTime? date,
-            int? activityId, double? duration, 
-            Entry.IntensityLevel? intensity, 
-            bool? exclude, string notes)
-        {            
-            //ViewBag.Date = ModelState["Date"].Value.AttemptedValue;
-            //ViewBag.ActivityId = ModelState["ActivityId"].Value.AttemptedValue;
-            //ViewBag.Duration = ModelState["Duration"].Value.AttemptedValue;
-            //ViewBag.Intensity = ModelState["Intensity"].Value.AttemptedValue;
-            //ViewBag.Exclude = ModelState["Exclude"].Value.AttemptedValue;
-            //ViewBag.Notes = ModelState["Notes"].Value.AttemptedValue;
-            
-            //*****This commented out code is only necessary without HTML Helper Methods in the Add.cshtml view
+        public ActionResult Add(Entry entry)
+        {   
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.AddEntry(entry);
 
-            return View();
+                // TODO Display the Entries list page
+            }
+            return View(entry);
         }
 
         public ActionResult Edit(int? id)
